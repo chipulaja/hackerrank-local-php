85706c2236fc46ddfdfc1c0ebfc0ea0477a0fc6a
chipulaja <chipul.aja@gmail.com>
2020-06-03 16:45:27 +0000

    change input and output path

--- run.sh
+++ run.sh
@@ -15,20 +15,21 @@ elif [ ! -f $1 ]; then
 else
     clear;
     PROJECT_DIR="$(cd "$(dirname "$1")"; pwd)";
-    CASE_INPUT="$PROJECT_DIR/case_input/*";
-    CASE_OUTPUT="$PROJECT_DIR/case_output";
+    CASE_INPUT="$PROJECT_DIR/input/input*";
+    CASE_OUTPUT="$PROJECT_DIR/output";
     mkdir -p "$PROJECT_DIR/temp/";
     
     for INPUT in $CASE_INPUT; do
-        FILENAME="$(basename -- $INPUT)";
-        OUTPUT="$PROJECT_DIR/temp/$FILENAME";
-        TEST="$CASE_OUTPUT/$FILENAME";
+        FILENAME_INPUT="$(basename -- $INPUT)";
+        FILENAME_OUTPUT=$(echo "$FILENAME_INPUT" | sed "s/input/output/")
+        OUTPUT="$PROJECT_DIR/temp/$FILENAME_OUTPUT";
+        TEST="$CASE_OUTPUT/$FILENAME_OUTPUT";
         cat $INPUT | OUTPUT_PATH="$OUTPUT" php $1;
         truncate -s -1 $OUTPUT;
         if cmp -s "$TEST" "$OUTPUT"; then
-            echo -e ${grn}$FILENAME success.${grn}${end};
+            echo -e ${grn}$FILENAME_INPUT success.${grn}${end};
         else
-            echo -e ${red}$FILENAME failed.${red}${end};
+            echo -e ${red}$FILENAME_INPUT failed.${red}${end};
             diff $TEST $OUTPUT
         fi
     done
